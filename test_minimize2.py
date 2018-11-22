5e2bd3ad32b8884a32024a2f3da9b2ae79664c92
Artem Pilkevich <artem.pilkevich@gmail.com>
2018-11-22 11:17:00 +0300

    Added simple tests.

--- test_minimize2.py
+++ test_minimize2.py
@@ -4,68 +4,7 @@ from scipy.optimize import fmin_cobyla
 import numpy as np
 
 import radar_potential_saushkin as rpt_saushkin
-
-
-def wrap_(funcs, variable_parameters, y_values, fixed_x_dict, bounds=None):
-    """
-
-    Args:
-        funcs (list(func, )): List with functions.
-        variable_parameters  (list(list, )): List with names varible parameters.
-        y_values  (list(floats, )): List with 'y' values.
-        fixed_x_dict  (list(dict, )): Dict with init parameters ({'x':1, 'xx':2})
-        bounds  (list(tuple, )|None):  Bounds.
-
-    Returns (list(dict, )|None):
-
-    """
-    spisok=[]
-    spisok.append(funcs)
-    spisok.append(variable_parameters)
-    spisok.append(y_values)
-    spisok.append(fixed_x_dict)
-
-    list_var = list()
-    boundsss = list()
-    for i in range(len(variable_parameters)):
-        for j in range(len(variable_parameters[i])):
-            if(list_var.count(variable_parameters[i][j]) == 0):
-                list_var.append(variable_parameters[i][j])
-                boundsss.append(list(bounds[i][j]))
-            for k in range(len(list_var)):
-                if(list_var[k] == variable_parameters[i][j]):
-                    if(boundsss[k][0] < bounds[i][j][0]):
-                        boundsss[k][0] = bounds[i][j][0]
-                    if(boundsss[k][1] > bounds[i][j][1]):
-                        boundsss[k][1] = bounds[i][j][1]
-
-    func_ = wrapper_for_minimization(funcs, variable_parameters,
-                                     y_values, fixed_x_dict, list_var)
-
-    cons = []
-    for i in range(len(boundsss)):
-        cons = cons + [lambda x, i_local=i: x[i_local] - boundsss[i_local][0]]
-        cons = cons + [lambda x, i_local=i: -x[i_local] + boundsss[i_local][1]]
-    for _ in range(1000):
-        result = fmin_cobyla(func_, 1e0 * np.random.rand(len(variable_parameters[0])), cons=cons)#, rhoend=1)
-        return dict(zip(variable_parameters[0], result))
-
-
-def func1(args,funcs, variable_parameters, y_values, fixed_x_dict, list_var):
-    arr = np.zeros(len(funcs))
-    for i in range(len(funcs)):
-        arg = dict()
-        for j in range(len(list_var)):
-            for k in range(len(variable_parameters[i])):
-                if(list_var[j] == variable_parameters[i][k]):
-                    arg.update({list_var[j] : args[j]})
-        arr[i] = np.power(y_values[i] - funcs[i](**arg,**fixed_x_dict[i]),2)
-    return arr
-
-
-def wrapper_for_minimization(func, variable_parameters, y_values, fixed_x_dict, list_var):
-    funccc = lambda args: np.sum(func1(args,func, variable_parameters, y_values, fixed_x_dict, list_var))
-    return funccc
+from test_minimize1 import wrap_
 
 
 class MyTest(TestCase):
@@ -124,6 +63,28 @@ class MyTest(TestCase):
             print(func_result2,  'res2= ', func_result4)
         print(wrap_result)
 
+    def test_call_without_args(self):
+        """ Проверка, что без при вызове без аргументов вознкает TypeError, если ошибки
+            не было, тест завершится с assert
+
+        Returns:
+
+        """
+        try:
+            wrap_()
+        except TypeError:
+            pass
+        else:
+            assert False
+
+    def test_call_with_empty_list(self):
+        """ Проверка, можно ли вызывать функцию с пустыми списами. В докстринге wrap_
+            нет на это ограничений.
+
+            Видим что падает с IndexError. Значит нужно добавить проверок в wrap_,
+            или хотябы поправить описание.
+        """
+        wrap_([], [], [], [], [])
 
 
 
